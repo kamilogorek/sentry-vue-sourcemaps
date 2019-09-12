0655592a89a6fa863864f837f4fe5bc78bb71188
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-09-12 12:14:56 +0200

    added SDK

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <button v-on:click="whoops">Click me to break stuff!</button>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
@@ -31,10 +32,23 @@
 </template>
 
 <script>
+function foo () {
+  bar()
+}
+
+function bar () {
+  throw new Error('whoops')
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    whoops: function () {
+      foo()
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Sentry.init({
+  debug: true,
+  dsn: "<your-dsn>",
+  integrations: [
+    new VueIntegration({
+      Vue,
+      logErrors: true
+    })
+  ]
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
